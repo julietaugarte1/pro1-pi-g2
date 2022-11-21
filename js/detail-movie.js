cce9a082ce2b4cceebb4ce43c6a89534b0e45e38
julietaugarte1 <jugarte@udesa.edu.ar>
2022-11-21 17:25:23 -0300

    juli

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,14 +1,13 @@
 // capturar elementos del DOM
 let qs = location.search;
 let qsObj = new URLSearchParams(qs);
-let pelicula = qsObj.get("idpersonaje");
+let pelicula = qsObj.get("id");
 
 //crear una variable y despues seleccionar del html
 let imagen = document.querySelector(".portadadetail")
 let titulo = document.querySelector(".titulo");
 let calificacion = document.querySelector(".calificacion");
 let estreno = document.querySelector(".estreno");
-let duracion = document.querySelector(".")
 let sinopsis = document.querySelector(".sinopsis");
 let genero = document.querySelector(".titulospeliculas");
 
@@ -17,17 +16,19 @@ let apiKey = "996dc0a073c9e126288abaa1ade3770b"
 let urldetail = `https://api.themoviedb.org/3/movie/${pelicula}?api_key=${apiKey}&language=en-US`  //si o si con comas invertidas
 
 //
-fetch (urldetail-movie)
+fetch(urldetail)
     .then (function(response){
-        return repsuesta.json()
+        return response.json()
     })
     .then (function(data){
         console.log(data)
+        let generosPeliculas = ""
 
         for (let i = 0 ; i < data.genres.lenght ; i++){
             generosPeliculas += ` <li> <a class="generos" href="./genres.html">${data.genres[i].name}</a> </li>`
         }
-        titulo.innerText = data.original;
+
+        titulo.innerText = data.title;
         imagen.src = data.image;
         calificacion.innerText = data.vote;
         estreno.innerText = data.release;
--- js/index.js
+++ js/index.js
@@ -1,104 +1,98 @@
-let apiKey = "996dc0a073c9e126288abaa1ade3770b"
-let urlseries = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`
-let urlpelis = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-let urlsugerencias = `https://api.themoviedb.org/3/tv/{tv_id}/recommendations?api_key=${apiKey}&language=en-US&page=1`
+let apiKey = "996dc0a073c9e126288abaa1ade3770b";
 
-let bloque1 = document.querySelector(".bloque1")
-let bloque2 = document.querySelector(".bloque2")
-let bloque3 = document.querySelector(".bloque3")
+let url_series = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`;
+let url_popular_movies = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+let url_top_movies = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
 
-//BLOQUE 1
-fetch(urlsugerencias)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        let sugerencias = " " ;
-        console.log(data)
-        for (let i = 0 ; i < 5 ; i++){
-            console.log(data.results[i]);
+let recommended_series = document.querySelector(".recommended_series");
+let recommended_movies = document.querySelector(".recommended_movies");
+let top_movies = document.querySelector(".top_movies");
 
-            let titulo = data.results[i].title
-            let imagenes = data.results[i].poster_path
-            let id = data.results[i].id
-            let fecha = data.results[i].release_data
+//BLOQUE 1 (popular series)
+fetch(url_series)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let sugerencias = "";
+    console.log(data);
+    for (let i = 0; i < 5; i++) {
+      let titulo = data.results[i].name;
+      let imagenes = data.results[i].poster_path;
+      let id = data.results[i].id;
+      let fecha = data.results[i].first_air_date;
 
-            sugerencia +=`<article class="bloque-portada"> 
+      sugerencias += `<article class="bloque-portada"> 
+                            <a class="portadahome" href="./detail-serie.html?id=${id}">
+                            <img class="portada" src="https://image.tmdb.org/t/p/w500/${imagenes}">
+                            <p class="texto-portada"> <a class="titulospeliculas" href="./detail-movie.html"> ${titulo} </a>  </p>
+                            <p class="fecha-portada"> ${fecha}</p>
+                            </a>
+                          </article>`;
+    }
+    recommended_series.innerHTML = sugerencias;
+    return data;
+  })
+  .catch(function (error) {
+    console.log(error);
+    return error;
+  });
+
+//BLOQUE 2 (popular movies)
+fetch(url_popular_movies)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let movies = " ";
+    for (let i = 0; i < 5; i++) {
+      let titulo = data.results[i].title;
+      let imagenes = data.results[i].poster_path;
+      let id = data.results[i].id;
+      let fecha = data.results[i].release_date;
+
+      movies += ` <article class="bloque-portada"> 
                             <a class="portadahome" href="./detail-movie.html?idPersonaje=${id}">
                             <img class="portada" src="https://image.tmdb.org/t/p/w500/${imagenes}">
                             <p class="texto-portada"> <a class="titulospeliculas" href="./detail-movie.html"> ${titulo} </a>  </p>
                             <p class="fecha-portada"> ${fecha}</p>
                             </a>
-                          </article>`
-        }
-        bloque1.innerHTML = sugerencias;
-        return data
-    })
-    .catch(function(error){
-        console.log(error)
-        return error
-    })
+                        </article>`;
+    }
+    recommended_movies.innerHTML = movies;
+    return data;
+  })
+  .catch(function (error) {
+    console.log(error);
+    return error;
+  });
 
+//BLOQUE 3 (top movies)
+fetch(url_top_movies)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let series = "";
 
-//BLOQUE 2
-fetch(urlpelis)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        let pelis = " " ;
-        for (let i = 0 ; i < 5 ; i++){
-            console.log(data.results[i]);
+    for (let i = 0; i < 5; i++) {
+      let titulo = data.results[i].title;
+      let imagenes = data.results[i].poster_path;
+      let id = data.results[i].id;
+      let fecha = data.results[i].release_date;
 
-            let titulo = data.results[i].title
-            let imagenes = data.results[i].poster_path
-            let id = data.results[i].id
-            let fecha = data.results[i].release_data
-
-            pelis += ` <article class="bloque-portada"> 
-                            <a class="portadahome" href="./detail-movie.html?idPersonaje=${id}">
-                            <img class="portada" src="https://image.tmdb.org/t/p/w500/${imagenes}">
-                            <p class="texto-portada"> <a class="titulospeliculas" href="./detail-movie.html"> ${titulo} </a>  </p>
-                            <p class="fecha-portada"> ${fecha}</p>
-                            </a>
-                        </article>`
-        }
-        bloque2.innerHTML = movies;
-        return data
-    })
-    .catch(function(error){
-        console.log(error)
-        return error
-    })
-
-
-//BLOQUE 3
-fetch(urlseries)
-.then(function(response){
-    return response.json()
-})
-.then(function(data){
-    let series = " " ;
-    for (let i = 0 ; i < 5 ; i++){
-        console.log(data.results[i]);
-
-        let titulo = data.results[i].title
-        let imagenes = data.results[i].poster_path
-        let id = data.results[i].id
-        let fecha = data.results[i].release_data
-
-        series +=  ` <article class="bloque-portada"> 
-                        <a class="portadahome" href="./detail-movie.html?idPersonaje=${id}">
+      series += ` <article class="bloque-portada"> 
+                        <a class="portadahome" href="./detail-movie.html?id=${id}">
                         <img class="portada" src="https://image.tmdb.org/t/p/w500/${imagenes}">
                         <p class="texto-portada"> <a class="titulospeliculas" href="./detail-movie.html"> ${titulo} </a>  </p>
                         <p class="fecha-portada"> ${fecha}</p>
                         </a>
-                     </article>`
+                     </article>`;
     }
-    bloque3.innerHTML = series;
-    return data
-})
-.catch(function(error){
-    console.log(error)
-    return error
-})
+    top_movies.innerHTML = series;
+    return data;
+  })
+  .catch(function (error) {
+    console.log(error);
+    return error;
+  });
