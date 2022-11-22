07736b8135284d10004b30ce486033953a628af3
julietaugarte1 <jugarte@udesa.edu.ar>
2022-11-22 09:34:08 -0300

    cambios juli

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -13,7 +13,8 @@ let genero = document.querySelector(".titulospeliculas");
 
 //crear una apikey para poder utilizar url
 let apiKey = "996dc0a073c9e126288abaa1ade3770b"
-let urldetail = `https://api.themoviedb.org/3/movie/${pelicula}?api_key=${apiKey}&language=en-US`  //si o si con comas invertidas
+let urldetail = `https://api.themoviedb.org/3/movie/${pelicula}?api_key=996dc0a073c9e126288abaa1ade3770b&language=en-US`  //si o si con comas invertidas
+
 
 //
 fetch(urldetail)
@@ -22,22 +23,67 @@ fetch(urldetail)
     })
     .then (function(data){
         console.log(data)
-        let generosPeliculas = ""
-
-        for (let i = 0 ; i < data.genres.lenght ; i++){
-            generosPeliculas += ` <li> <a class="generos" href="./genres.html">${data.genres[i].name}</a> </li>`
-        }
-
-        titulo.innerText = data.title;
-        imagen.src = data.image;
-        calificacion.innerText = data.vote;
-        estreno.innerText = data.release;
-        sinopsis.innerText = data.overview;
-        genero.innerHTML = generosPeliculas;
-
-        return data
+        
+        let generosPeliculas = document.querySelector('.contenedor')
+        let contenido =
+         `<article class= "bloquedetail"> <li> <a class="generos" href="./detail-movie.html">${data.genres[i].name}</a> </li>
+         <p class = "texto"> <u>Titulo</u>: ${data.title} </p> <img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${data.poster_path}" alt='' /> 
+         <p class = "texto"> <u>Fecha de estreno</u>: ${data.release_date} </p>
+         <p class = "texto" > <u>Duración</u>: ${data.runtime} minutos </p>
+         <p class = "texto" > <u>Calificación</u>: ${data.vote_average} </p>
+         <p class = "texto" > <u>Sinópsis</u>: ${data.overview} </p>
+         </article>
+         `
+      
+        generosPeliculas += contenido
     })
     .catch(function(error){
         console.log(error)
         return error
     })
+
+
+/* let qs = location.search;
+let qsObj = new URLSearchParams(qs);
+let id = movieobjeto.get('id');
+console.log(id)
+
+//crear una variable y despues seleccionar del html
+let imagen = document.querySelector(".portadadetail")
+let titulo = document.querySelector(".titulo");
+let calificacion = document.querySelector(".calificacion");
+let estreno = document.querySelector(".estreno");
+let sinopsis = document.querySelector(".sinopsis");
+let genero = document.querySelector(".titulospeliculas");
+
+//crear una apikey para poder utilizar url
+let apiKey = "996dc0a073c9e126288abaa1ade3770b"
+let urldetail = `https://api.themoviedb.org/3/movie/${pelicula}?api_key=${apiKey}&language=en-US`  //si o si con comas invertidas
+
+
+fetch(urldetail)
+    .then(function (respuesta) {
+        return respuesta.json();
+    })
+
+    .then(function (data) {
+        console.log(data);
+        let container = document.querySelector('.contenedor')
+        let contenido =
+
+            `<article class= "contenedor">
+        <p class = "texto"> <u>Titulo</u>: ${data.title} </p> <img class = "fotofast" src= "https://image.tmdb.org/t/p/w500${data.poster_path}" alt='' /> 
+        <p class = "texto"> <u>Fecha de estreno</u>: ${data.release_date} </p>
+        <p class = "texto" > <u>Duración</u>: ${data.runtime} minutos </p>
+        <p class = "texto" > <u>Calificación</u>: ${data.vote_average} </p>
+        <p class = "texto" > <u>Sinópsis</u>: ${data.overview} </p>
+        </article>
+        `;
+        container.innerHTML += contenido
+    })
+
+
+    .catch(function (error) {
+        console.log(error);
+
+    }) */
