426fcde8f6db699bdbf8115a8ef79752ee797c56
julietaugarte1 <jugarte@udesa.edu.ar>
2022-11-23 23:51:30 -0300

    juli

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,60 +1,74 @@
-/* En este el titulo se cambia segun el genero pero las peliculas son siempre las mismas */
+/* En este funciona la seleccion de peliculas por genero Pero el titulo no se cambia */
+
 
 let apiKey = "996dc0a073c9e126288abaa1ade3770b";
-let queryString = location.search;
-let Obj = new URLSearchParams(queryString);
-let id_movie = Obj.get('id');
+
+let querystring = location.search;
+let query2 = new URLSearchParams(querystring)
+let idGeneo = query2.get("id")
+let nombreGenero = query2.get('name')
+let type =query2.get("type")
 
 
+let url = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&sort_by=popularity.desc&with_genres=${idGeneo}&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
 
-let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-
-let listaGeneros = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
-
-fetch(listaGeneros)
-    .then(function(response){
+if(type=="movies"){
+    fetch(url)
+    .then(function (response) {
         return response.json()
     })
-    .then(function(data){
+    .then(function (data) {
+        console.log(data);
+        let tituloGenero = document.querySelector(".titulo_genero");
+        let listaGenero=document.querySelector(".listaGenero");
+        tituloGenero.innerHTML+=`<h1> Peliculas del genero: ${nombreGenero}`
         
-        let generos = data.genres 
-        let title = document.querySelector(".titulo_genero")
-        let titulo = ""
-    
-        for (let i = 0; i < generos.length; i++) {
-            if (id_movie == generos[i].id){ 
-                titulo += `<h2>${generos[i].name}</h2>`
-                title.innerHTML = titulo
-        }}
-        console.log(titulo);
+        for (let i = 0; i < data.results.length ; i++) {
+            listaGenero.innerHTML += 
 
-        fetch(url)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-            let listaGenero=document.querySelector(".listaGenero");
-
-            for (let i = 0; i < data.results.length ; i++) {
-                listaGenero.innerHTML += 
-                    
                     `<article class="bloque-portada"> 
-                            <a class="portadahome" href="detail-movie.html?id=${data.results[i].id}">
-                            <img class="portada" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
-                            <p class="texto-portada"> <a class="titulospeliculas" href="./detail-movie.html">  ${data.results[i].title} </a>  </p>
-                            <p class="fecha-portada"> Fecha de estreno: ${data.results[i].release_date}</p>
-                            </a>
-                          </article>`
-                
-                
-            }
-          })
-
-          .catch(function (error) {
-          console.log(error)
-          })
-    
-        
+                    <a class="portadahome" href="detail-movie.html?id=${data.results[i].id}">
+                    <img class="portada" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
+                    <p class="texto-portada"> <a class="titulospeliculas" href="./detail-movie.html">  ${data.results[i].title} </a>  </p>
+                    <p class="fecha-portada"> Fecha de estreno: ${data.results[i].release_date}</p>
+                    </a>
+                </article>`
+            
+        }
 
     })
+    .catch(function (error) {
+        console.log(error)
+    })
+
+}
+
+else{
+    fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+        let tituloGenero = document.querySelector(".titulo_genero");
+        let listaGenero=document.querySelector(".listaGenero");
+        tituloGenero.innerHTML+=`<h1> Series del genero: ${nombreGenero}
+        </h1>`
+        for (let i = 0; i < data.results.length ; i++) {
+            listaGenero.innerHTML +=
+        `<article class="bloque-portada"> 
+                    <a class="portadahome" href="detail-movie.html?id=${data.results[i].id}">
+                    <img class="portada" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
+                    <p class="texto-portada"> <a class="titulospeliculas" href="./detail-movie.html">  ${data.results[i].name} </a>  </p>
+                    <p class="fecha-portada"> Fecha de estreno: ${data.results[i].first_air_date}</p>
+                    </a>
+                </article>`
+               
+    
+        }
+
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+}
--- js/search-results.js
+++ js/search-results.js
@@ -1,77 +1,76 @@
 let apiKey = "996dc0a073c9e126288abaa1ade3770b";
 
-let qs = location.search; 
-let qsObj = new URLSearchParams(qs) ;
-let peliculas = qsObj.get('busqueda') ; 
-let tipo = qsObj.get('media'); 
+let qs = location.search;
+let qsObj = new URLSearchParams(qs);
+let peliculas = qsObj.get("busqueda");
+let tipo = qsObj.get("media");
+let busqueda = document.querySelector(".search");
+let palabraPelicula = document.querySelector('#palabraPelicula');
+let sectionDetail = document.querySelector('.bloquedetail');
+let resultadoContainer = document.querySelector('.bloquedetail');
+let pelicula;
+let empty = document.querySelector(".empty");
 
-if (tipo == 'all' || tipo == 'movies') {
+busqueda.addEventListener('submit', e => {
+    e.preventDefault();
+    pelicula = palabraPelicula.value;
+    console.log(pelicula);
+    buscar(pelicula);
+    palabraPelicula.value = "";
+});
 
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${qsObj}`
+function buscar(pelicula){
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${pelicula}&page=1`; // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+    let buscados = "";
 
     fetch(url)
-        .then(function (response){
-            return response.json()
-        })
-
-        .then(function(data) {
-            console.log(data);
-            let informacion = data.results
-            let buscados = ''
-            if (informacion.lenght == 0 ) {
-                let empty = document.querySelector('.empty')
-                empty.innerText = `No se encontraron resultados para ${qsObj}`
-
-            }
-            else{
-                for (let i = 0; i < informacion.lenght; i++) {
-                    if (informacion[1].poster_path == null) {
-                        buscados += `<article class="bloquedetail">
-                        <a href="./detail-movie.html?movie_id=${informacion[i].id}"> 
-                        <img class="portadadetail" src="https://image.tmdb.org/t/p/w500/${informacion[i].poster_path}" alt="Portada">
-                        </a>
-                        <div class="resultados">
-                        <a href="detailmovie.html">
-                        <h2 class="titulo-movie-search">${informacion[i].title}</h2>
-                        </a>
-                        <h3 class="estreno-search"> Fecha de estreno: ${informacion[i].release_date}</h3>
-                        <p class="sinopsis-search">${informacion[i].overview}</p>
-                        </div>
-                        </article>`
-
-                    }
-
-                    else {
-                        buscados +=
-                            `<article class="bloquedetail">
-                            <a href="./detail-movie.html?movie_id=${informacion[i].id}"> 
-                            <img class="portadadetail" src="https://image.tmdb.org/t/p/w500/${informacion[i].poster_path}" alt="Portada">
-                            </a>
-                            <div class="resultados">
-                            <a href="detailmovie.html">
-                            <h2 class="titulo-movie-search">${informacion[i].title}</h2>
-                            </a>
-                            <h3 class="estreno-search"> Fecha de estreno: ${informacion[i].release_date}</h3>
-                            <p class="sinopsis-search">${informacion[i].overview}</p>
-                            </div>
-                            </article>`
-
-                    }
+      .then(function (response) {
+        return response.json();
+      })
+  
+      .then(function (data) {
+        console.log(data);
+        let informacion = data.results;
+        if (informacion.lenght == 0) {
+          empty.innerText = `No se encontraron resultados para ${pelicula}`;
+        } else {
+            informacion.forEach(el =>{
+                console.log('pase')
+                if (el.poster_path == null) {
+                  buscados += `<article class="bloquedetail">
+                              <a href="./detail-movie.html?movie_id=${el.id}"> 
+                              <img class="portadadetail" src="https://image.tmdb.org/t/p/w500/${el.poster_path}" alt="Portada">
+                              </a>
+                              <div class="resultados">
+                              <a href="detailmovie.html">
+                              <h2 class="titulo-movie-search">${el.title}</h2>
+                              </a>
+                              <h3 class="estreno-search"> Fecha de estreno: ${el.release_date}</h3>
+                              <p class="sinopsis-search">${el.overview}</p>
+                              </div>
+                              </article>`;
+                } else {
+                  buscados += `<article class="bloquedetail">
+                                  <a href="./detail-movie.html?movie_id=${el.id}"> 
+                                  <img class="portadadetail" src="https://image.tmdb.org/t/p/w500/${el.poster_path}" alt="Portada">
+                                  </a>
+                                  <div class="resultados">
+                                  <a href="detailmovie.html">
+                                  <h2 class="titulo-movie-search">${el.title}</h2>
+                                  </a>
+                                  <h3 class="estreno-search"> Fecha de estreno: ${el.release_date}</h3>
+                                  <p class="sinopsis-search">${el.overview}</p>
+                                  </div>
+                                  </article>`;
                 }
-            }
-        let cap = document.querySelector('h1')
-        cap.innerText = `Resultados de Busqueda : ${qsObj}`;
-
+            })
+            let cap = document.querySelector("h1");
+            cap.innerText = `Resultados de Busqueda : ${pelicula}`;
+          }
+          resultadoContainer.insertAdjacentHTML('beforeend',buscados);
+          buscados = "";
         })
-
-    .catch(function (error){
-        console.log('Error:'+ error)
-
-    })
-
-window.addEventListener('load',function(e){
-    let gif = document.querySelector ('.gif')
-    gif.getElementsByClassName.display = "none";
-    })
+      .catch(function (error) {
+        console.log("Error:" + error);
+      });
 }
-
