67335f7526049f5faeec41d052f63f99ac111113
luciacupito <lcupito@udesa.edu.ar>
2022-11-23 21:30:50 -0300

    lula

--- js/validar-form.js
+++ js/validar-form.js
@@ -1,6 +1,6 @@
 /* let search = document.querySelector('.search')
 let input = document.querySelector('.palabraPelicula')
- 
+ git
 search.addEventListener('submit' , function(e) {
     e.preventDefault();
 
